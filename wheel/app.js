7ded0bf99b124702e7bb203420de3eadc28f3325
byhaejin <byhaejin@gmail.com>
2017-03-13 12:59:35 +0900

    project es6

---
+++ wheel/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+
+const _wheelView = new WheelView($('#wheel-view'), {
+    init: {
+        display: false
+    },
+    event: {
+        select: (e) => {
+
+        }
+    }
+});
+
+
+
+
+$(function(){
+    /** start page */
+    _wheelView.display(true);
+
+
+    $('.header-logo img').click(function(){
+        location.reload();
+    });
+});
